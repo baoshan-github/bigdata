2f8a11256bf862baf6752cbc05eafe26d8512860
baoshan <zhzhang09@126.com>
2015-11-03 00:43:31 +0800

    add git.cmd describes some git commands

---
+++ git.cmd
@@ -0,0 +1,48 @@
+git config --global user.name "Your Name"
+git config --global user.email "email@example.com"
+
+创建空的版本库
+mkdir zzh
+cd zzh
+git init
+
+初始化一个Git仓库，使用git init命令。
+添加文件到Git仓库，分两步：
+	第一步，使用命令git add <file>，注意，可反复多次使用，添加多个文件；
+	第二步，使用命令git commit，完成。
+
+git diff readme.txt
+git status
+git log
+git log --pretty=oneline
+
+回到上一版本
+git reset --hard HEAD^
+git reflog 记录每一次命令
+
+HEAD指向的版本就是当前版本，因此，Git允许我们在版本的历史之间穿梭，使用命令git reset --hard commit_id。
+	穿梭前，用git log可以查看提交历史，以便确定要回退到哪个版本。
+	要重返未来，用git reflog查看命令历史，以便确定要回到未来的哪个版本。
+
+把文件往Git版本库里添加的时候，是分两步执行的：
+	第一步是用git add把文件添加进去，实际上就是把文件修改添加到暂存区；
+	第二步是用git commit提交更改，实际上就是把暂存区的所有内容提交到当前分支。
+	
+Git跟踪并管理的是修改，而非文件。
+又理解了Git是如何跟踪修改的，每次修改，如果不add到暂存区，那就不会加入到commit中
+
+要关联一个远程库，使用命令git remote add origin git@server-name:path/repo-name.git；
+关联后，使用命令git push -u origin master第一次推送master分支的所有内容；
+
+要克隆一个仓库，首先必须知道仓库的地址，然后使用git clone命令克隆。
+Git支持多种协议，包括https，但通过ssh支持的原生git协议速度最快。
+
+https://github.com/baoshan-github/bigdata.git
+
+Git鼓励大量使用分支：
+	查看分支：git branch
+	创建分支：git branch <name>
+	切换分支：git checkout <name>
+	创建+切换分支：git checkout -b <name>
+	合并某分支到当前分支：git merge <name>
+	删除分支：git branch -d <name>
